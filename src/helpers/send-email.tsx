8b3d2bde950d26fc104d48bb9d50ccd99dc685f3
Airon Herbert <aironherbertms@gmail.com>
2024-11-20 11:06:37 -0300

    feat: remoção de sendEmail

--- src/helpers/send-email.tsx
+++ src/helpers/send-email.tsx
@@ -3,11 +3,6 @@ import { RefObject } from "react";
 
 export default function sendEmail(form: RefObject<HTMLFormElement>) {
   if (!form.current) return;
-  return emailjs.sendForm(
-    process.env.REACT_APP_SERVICE_ID || "",
-    process.env.REACT_APP_TEMPLATE_ID || "",
-    form.current,
-    process.env.REACT_APP_PUBLIC_KEY || ""
-  );
+  return null;
 }
 
